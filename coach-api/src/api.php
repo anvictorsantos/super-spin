22e441c4b7211385acc30b780e970c711403c73e
Antonio Pontes <anvictorsantos@gmail.com>
2025-01-18 10:32:08 +0000

    fix: update filter and sort parameters to default to empty strings

--- coach-api/src/api.php
+++ coach-api/src/api.php
@@ -4,8 +4,8 @@ require_once 'filter_and_sort.php';
 
 header('Content-Type: application/json');
 
-$filter = $_GET['filter'] ?? [];
-$sort = $_GET['sort'] ?? [];
+$filter = $_GET['filter'] ?? '';
+$sort = $_GET['sort'] ?? '';
 
 $coaches = getCoaches();
 $result = filterAndSort($coaches, $filter, $sort);
